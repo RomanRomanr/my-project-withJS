a8bd237c2c522356c9c10ef7a4dadd5ffafca26b
Valentyn <manilchuk.v@gmail.com>
2026-04-03 19:01:53 +0300

    Add JS to close mobile menu and disable page scroll

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle('is-open');
+    document.body.classList.add('menu-open');
     if (refs.modal.classList.contains('is-open')) {
       refs.openModalBtn.style.display = 'none';
     } else {
@@ -29,8 +30,20 @@
     }
   }
 
+  document.addEventListener('click', e => {
+    // якщо меню відкрите і клік НЕ всередині контейнера меню
+    if (
+      refs.modal.classList.contains('is-open') &&
+      !e.target.closest('.mobile-menu-container') &&
+      !e.target.closest('[data-menu-open]')
+    ) {
+      closeMenu();
+    }
+  });
+
   function closeMenu() {
     refs.modal.classList.remove('is-open');
     refs.openModalBtn.style.display = 'flex';
+    document.body.classList.remove('menu-open');
   }
 })();
