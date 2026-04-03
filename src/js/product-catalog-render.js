c08aa55a0dc835d7af0ec63e66ffb88e2b7d9100
alina.vinnik <alinavinnikhgfff@gmail.com>
2026-04-03 12:54:15 +0300

    add css

--- src/js/product-catalog-render.js
+++ src/js/product-catalog-render.js
@@ -1,17 +1,17 @@
-function itemTemplate(item) {
-  const { images, name, price, color } = item;
-  return ` <li class="furniture-catalog-items">
+export function createCatalogueFurniture(items) {
+  function itemTemplate(item) {
+    const { images, name, price, color } = item;
+    return ` <li class="furniture-catalog-items">
           <img class="furniture-catalog-img" src="${images[0]}" alt="${name}" width="500px" />
-          <h2>${name}</h2>
+          <h2 class="furniture-catalog-title">${name}</h2>
           <ul class='color-list'>
             ${color.map(c => `<li class='color-items' style="background-color: ${c};"></li>`).join('')}
           </ul>
-          <p>${price} грн</p>
-          <button class="btn-furniture-catalog" type="button">детальніше</button>
+          <p class="furniture-catalog-price">${price} грн</p>
+          <button class="furniture-catalog-btn" type="button">детальніше</button>
         </li>
         `;
-}
+  }
 
-function itemsTemplate(items) {
   return items.map(itemTemplate).join('');
 }
