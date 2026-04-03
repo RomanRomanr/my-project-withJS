5fd25f30ddb91509c4cccf4cb720d295b6613300
bohdan <bogdanovcharenko1997@gmail.com>
2026-04-03 20:31:22 +0300

    add fix and izitoast

--- src/js/product-filter.js
+++ src/js/product-filter.js
@@ -27,31 +27,46 @@ export function createCategories(categories) {
 
 
 
-// import {getListCategories, createCategories} from "./js/product-filter.js";
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
+
+// import { getListCategories, createCategories } from "./js/product-filter.js";
+// // import { getFurnitureByCategory, handleLoadCategory } from "./js/???.js";
 
 
 // document.addEventListener("DOMContentLoaded", async event => { 
-//   const categories = await getListCategories();
-//   const updatedCategories = [
+//     try { 
+//     const categories = await getListCategories();
+//     const updatedCategories = [
 //   {
 //     _id: "all",
 //     name: "Всі товари",
 //   },
 //   ...categories
 // ];
-//   createCategories(updatedCategories);
+//         createCategories(updatedCategories);
+//         // await handleLoadCategory();
 //  const firstItem = document.querySelector(".list-categories-item");
 // if (firstItem) {
 //   firstItem.classList.add("active-item-category");
 //   }
-// });
+        
+//     } catch (error) {
+//         iziToast.error({
+//         message: "Sorry, помилка при завантаженні категорій. Please try again!",
+//         position: "topRight",
+//       });
+//         console.error("Помилка при завантаженні категорій:", error);
+//     }
+//     });
+
 
 
 
 
 // const categoriesContainer = document.querySelector(".list-categories");
 
-// categoriesContainer.addEventListener("click", (event) => {
+// categoriesContainer.addEventListener("click", async (event) => {
   
 //   const categoryItem = event.target.closest(".list-categories-item");
 //   if (!categoryItem) return;
@@ -61,9 +76,20 @@ export function createCategories(categories) {
 //   categoryItem.classList.add("active-item-category");
 
 //   const categoryId = categoryItem.dataset.categoryId;
-//   if (categoryId === "all") {
-//   console.log("Показати всі товари");
-// } else {
-//     console.log(`Показати товари категорії: ${categoryId}`);
-// }
+//     try {
+//         if (categoryId === "all") {
+//             // await handleLoadCategory();
+//             console.log("Показати всі товари");
+//         } else {
+//             // await getFurnitureByCategory(categoryId, page = 1);
+//             console.log(`Показати товари категорії: ${categoryId}`);
+//         }
+//     } catch (error) { 
+//         iziToast.error({
+//         message: "Sorry, помилка при завантаженні товарів за категорією. Please try again!",
+//         position: "topRight",
+//       });
+//         console.error("Помилка при завантаженні товарів за категорією:", error);
+//     }
+    
 // });
