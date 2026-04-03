4770cdebe69a51b5e25392e66a3a29e7d92ec6e6
alina.vinnik <alinavinnikhgfff@gmail.com>
2026-04-03 12:54:15 +0300

    add css

---
+++ src/js/api-product-catalog.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+import { createCatalogueFurniture } from './product-catalog-render';
+
+const ulCataloge = document.querySelector('.furniture-catalog-list');
+
+let page = 1;
+
+// Fetch all catalog from API
+async function getAllFurniture(page) {
+  const apiResponse = await axios.get(
+    'https://furniture-store-v2.b.goit.study/api/furnitures',
+    {
+      params: {
+        page: page,
+        limit: 8,
+      },
+    }
+  );
+  return apiResponse.data;
+}
+
+export async function handleLoadCategory() {
+  const responce = await getAllFurniture(page);
+  const markup = createCatalogueFurniture(responce.furnitures);
+  ulCataloge.innerHTML = markup;
+}
+
+// Fetch catalog by category from API
+
+async function getFurnitureByCategory(category, page) {
+  const response = await axios(
+    'https://furniture-store-v2.b.goit.study/api/furnitures',
+    {
+      params: {
+        category: category,
+        page: page,
+        limit: 8,
+      },
+    }
+  );
+  return response.data;
+}
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import { handleLoadCategory } from './js/api-product-catalog.js';
+document.addEventListener('DOMContentLoaded', handleLoadCategory);
