d4e69089faad2caceb8089623ab3056a8f96485c
Yevhenii Chernov <chernovetskiy96@gmail.com>
2026-04-05 02:29:25 +0300

    ready for review and waiting for edits

--- src/js/product-modal-evente.js
+++ src/js/product-modal-evente.js
@@ -1,3 +1,42 @@
+// import 'star-rating.js/css';
+// import { getCategories, getFurnitureById, getFurnitures } from './js/furniture-api';
+// import { modalGallery, showModal } from './js/product-modal-render-functions';
+
+// async function testFurnitureApi() {
+//   try {
+//     const [furnituresResponse, categories] = await Promise.all([
+//       getFurnitures(),
+//       getCategories(),
+//     ]);
+
+//     const furnitures = furnituresResponse.furnitures ?? [];
+
+//     console.log('Список товарів:', furnitures);
+//     console.log('Категорії:', categories);
+
+//     if (furnitures.length === 0) {
+//       console.log('API повернув порожній список товарів.');
+//       return;
+//     }
+
+//     const firstFurnitureId = furnitures[5]._id;
+//     const product = await getFurnitureById(firstFurnitureId);
+
+//     console.log('Деталі першого товару:', product);
+
+//     modalGallery(product);
+//     showModal();
+//   } catch (error) {
+//     console.error('Помилка перевірки API меблів:', error);
+//   }
+// }
+
+// testFurnitureApi();
+
+
+
+
+
 import { getFurnitureById } from './furniture-api';
 import { hideModal, modalGallery, showModal } from './product-modal-render-functions';
 
