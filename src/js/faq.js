13060a334d00200f936d5e8230611ff4680e446b
Ign1s123 <genamisinskij@gmail.com>
2026-04-04 19:02:11 +0300

    Add accordion functionality to FAQ section and update styles

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,39 +1,21 @@
-const faqList = document.querySelector('.faq-list');
-const faqParagraphs = document.querySelectorAll('.faq-paragraph');
-const svgSpans = document.querySelectorAll('.faq-icon');
+import Accordion from "accordion-js";
+import "accordion-js/dist/accordion.min.css";
 
-let activeParagraph = false;
 
-faqList.addEventListener('click', (e) => {
-    if(e.target === e.currentTarget || e.target.tagName === `H3`) return;
-    
-    const faqParagraph = e.target.closest('.faq-item').querySelector('.faq-paragraph');
-    
-    if(faqParagraph.style.display === `none`){
-        faqParagraph.style.display = `block`;
-        activeParagraph = true;
-    }
-    else {
-        faqParagraph.style.display = `none`;
-        activeParagraph = false;
-    }
+new Accordion(".accordion-container", {
+  duration: 400,
+  
+  onOpen: (el) => {
+    const icon = el.querySelector(".faq-icon");
+    icon.innerHTML = `<svg width="32" height="32">
+      <use href="../img/icons.svg#icon-chevron-up"></use>
+    </svg>`;
+  },
 
-    if(activeParagraph){
-        faqParagraphs.forEach(paragraph => {
-            if(paragraph !== faqParagraph){
-                paragraph.style.display = `none`;
-            }
-        })
-    }
-
-   
-    svgSpans.forEach(span => {
-        const item = span.closest('.faq-item');
-        const paragraph = item.querySelector('.faq-paragraph');
-        if(paragraph.style.display === 'block'){
-            span.innerHTML = `<svg class="more-icon"  width="32" height="32"><use href = "../img/icons.svg#icon-chevron-up"></use></svg>`;
-        } else {
-            span.innerHTML = `<svg class="more-icon"  width="32" height="32"><use href = "../img/icons.svg#icon-chevron-down"></use></svg>`;
-        }
-    });
+  onClose: (el) => {
+    const icon = el.querySelector(".faq-icon");
+    icon.innerHTML = `<svg width="32" height="32">
+      <use href="../img/icons.svg#icon-chevron-down"></use>
+    </svg>`;
+  }
 });
